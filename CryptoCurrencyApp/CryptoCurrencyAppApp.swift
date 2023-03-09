77cc4685ef80621590be7b2c1879605334435b89
Jose Cepero <jose.cepero@applydigital.com>
2023-03-09 12:03:12 -0300

    Initial Commit

---
+++ CryptoCurrencyApp/CryptoCurrencyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrencyAppApp.swift
+//  CryptoCurrencyApp
+//
+//  Created by Jose Manuel Cepero on 09-03-23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrencyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
